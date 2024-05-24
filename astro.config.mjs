f7f79f786dcd9c72eb1537ccef2164bb7cf24aea
Skarabajo18 <56693615+Skarabajo18@users.noreply.github.com>
2024-05-24 16:02:37 -0400

    rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://blog-felipe-segovia.netlify.app/"
+  site: "https://blog-felipe-segovia.netlify.app/",
+  integrations: [preact()]
 });
