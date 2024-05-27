164dbf00614726620f934107a99e331da27dd2c3
Skarabajo18 <56693615+Skarabajo18@users.noreply.github.com>
2024-05-27 17:17:16 -0400

    tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
+import tailwind from '@astrojs/tailwind';
 import preact from "@astrojs/preact";
+
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog-felipe-segovia.netlify.app/",
-  integrations: [preact()]
+  integrations: [preact(), tailwind()],
 });
